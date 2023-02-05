d854bae25298f309b8eb991ee141d32ef274ecc9
oakeley <oakeley@users.noreply.github.com>
2023-02-05 23:40:25 +0100

    force to use watchdrip-mini 38962

--- utils/config/global-constants.js
+++ utils/config/global-constants.js
@@ -2,7 +2,8 @@ export const WF_INFO = "watchdrip_info";
 export const WF_INFO_LAST_UPDATE = "watchdrip_info_last"; 
 export const WF_INFO_LAST_UPDATE_SUCCESSFUL = "watchdrip_info_last_sucessful";
 
-export const WATCHDRIP_APP_ID = "28962";
+// force to use watchdrip-mini
+export const WATCHDRIP_APP_ID = "38962";
 export const WATCHDRIP_WF_ID = "38963";
 
 export const WATCHDRIP_CONFIG = "watchdrip_config";
